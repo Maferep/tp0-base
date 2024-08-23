f2cd4225414d1637aa320bd76e283caafd7b99f4
Maferep <mf.pont.tv@gmail.com>
2024-08-23 17:07:11 -0300

    ej1

---
+++ helper-scripts/generator.py
@@ -0,0 +1,50 @@
+import sys
+import io
+try:
+    n = int(sys.argv[2])
+    name = str(sys.argv[1])
+except:
+    print("Bad input")
+    exit()
+client_strings = []
+for number in range(1, n+1):
+    client = """\
+  client{}:
+    container_name: client{}
+    image: client:latest
+    entrypoint: /client
+    environment:
+    - CLI_ID=1
+    - CLI_LOG_LEVEL=DEBUG
+    networks:
+    - testing_net
+    depends_on:
+    - server
+""".format(number, number)
+    client_strings.append(client)
+
+client_strings = "".join(client_strings)
+
+full = """name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+{}
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+""".format(client_strings)
+
+fp = io.open(name, "w")
+fp.write(full)
+fp.close()
