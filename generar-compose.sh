dcb0acba03cd47b3bf7f54ee9aef152f15e4e775
Mafer <mf.pont.tv@gmail.com>
2025-03-21 14:43:06 -0300

    generator

---
+++ generar-compose.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 ./helper-scripts/generator.py $1 $2
---
+++ helper-scripts/generator.py
@@ -0,0 +1,48 @@
+import sys
+import io
+try:
+    n = int(sys.argv[2])
+    name = str(sys.argv[1])
+except:
+    print("Bad input")
+    exit()
+client_strings = []
+for number in range(1, n+1):
+    client = """\
+  client{}:
+    container_name: client{}
+    image: client:latest
+    entrypoint: /client
+    networks:
+    - testing_net
+    depends_on:
+    - server
+    volumes:
+      - ./client/config.yaml:/config.yaml
+""".format(number, number)
+    client_strings.append(client)
+
+client_strings = "".join(client_strings)
+
+full = """name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    networks:
+      - testing_net
+    volumes:
+      - ./server/config.ini:/config.ini
+{}
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+""".format(client_strings)
+
+fp = io.open(name, "w")
+fp.write(full)
+fp.close()
